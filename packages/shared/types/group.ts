2317c4564f0b4ba038ef50f33a600ae84d44b38e
Shin Akira <graynatsu80@gmail.com>
2025-12-27 17:45:31 +0700

    fix(comment) : fix tree view, UI and collapse comment \n fix(group):display all group

--- packages/shared/types/group.ts
+++ packages/shared/types/group.ts
@@ -2,3 +2,5 @@ import { Enums } from "./database.types";
 
 export type Privacy_Group = Exclude<Enums<"privacy_group">, "secret">;
 export type Membership_Mode = "auto" | "request";
+export type GroupMemberStatus = Enums<"member_status">;
+export type GroupMemberRole = Enums<"group_roles">;
